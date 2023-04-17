58d47a5358a3707441e6a7a7582939398472aa60
Asif A.S <86577020+Asifasif6625@users.noreply.github.com>
2023-04-17 13:02:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -567,9 +567,11 @@ async def cb_handler(client: Client, query: CallbackQuery):
          ]]
         try:
             if AUTH_CHANNEL and not await is_subscribed(client, query):
+                await query.answer('Unblock the bot mahn !', show_alert=True)
                 await query.answer(url=f"https://telegram.dog/{temp.U_NAME}?start={ident}_{file_id}")
                 return
             elif settings['botpm']:
+                await query.answer('Unblock the bot mahn !', show_alert=True)
                 await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
                 return
             else:
